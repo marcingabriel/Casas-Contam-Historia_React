daf8ade0c673ab687cb07786a782309a25e51fbf
Marcio Gabriel <marciogabriel.gs27@gmail.com>
2024-06-20 15:55:20 -0300

    Inicio de router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+
+import Home from './routes/Home.jsx'
+import Sobre from './routes/Sobre.jsx'
+import Error from './routes/Error.jsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      
+      {
+        path: "Sobre",
+        element: <Sobre />
+      },
+
+    ]
+
+
+    
+  }
+
+
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
