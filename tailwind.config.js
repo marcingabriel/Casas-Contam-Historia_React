52aaa9ca3b66850979c60dd8ce324d27f17430d8
Marcio Gabriel <marciogabriel.gs27@gmail.com>
2024-06-20 13:19:26 -0300

    Inicio

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const flowbite = require("flowbite-react/tailwind");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", "./src/**/*.{js,ts,jsx,tsx}", 
+    flowbite.content(),
+    'node_modules/flowbite-react/lib/esm/**/*.js'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flowbite.plugin(),
+    require('flowbite/plugin')
+  ],
+};
