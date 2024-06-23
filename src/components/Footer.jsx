48361a0edb1a240d8e0a3e4d69187395081480b7
Marcio Gabriel <marciogabriel.gs27@gmail.com>
2024-06-23 12:35:41 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,14 @@ const Footer = () => {
     <footer className="bg-white rounded-lg shadow dark:bg-gray-800 absolute  left-0 w-full">
       <div className="container mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2024 <a href="https://flowbite.com/" className="hover:underline">Marcio Gabriel</a>. All Rights Reserved.
+          © 2024 <a href="https://www.linkedin.com/in/marcio-gabriel-gonçalves-soares-133716221" className="hover:underline">Marcio Gabriel</a>. All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">Sobre</a>
+            <a href="https://github.com/marcingabriel" className="hover:underline hover:text-purple-800 me-4 md:me-6">GitHub</a>
           </li>
           <li>
-            <a href="#" className="hover:underline">Contato</a>
+            <a href="https://www.linkedin.com/in/marcio-gabriel-gonçalves-soares-133716221" className="hover:underline">Contato</a>
           </li>
         </ul>
       </div>
