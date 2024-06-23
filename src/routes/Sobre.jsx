1eef358fe1c2953dd4fc610559c1acdbf6f96026
Marcio Gabriel <marciogabriel.gs27@gmail.com>
2024-06-23 12:23:39 -0300

    update sobre nos

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,44 +1,46 @@
 import React from 'react';
 import {Component} from '../components/CardIntegrantes'
 import { Prof } from '../components/CardProf';
+import useAnimateOnScroll from '../components/Animation';
 
 const Sobre = () => {
+  useAnimateOnScroll();
   return (
     <div className="font-sans text-gray-900 leading-normal tracking-wider bg-cover">
       <div className="flex items-center justify-center min-h-screen">
         <div className="max-w-6xl flex items-center h-auto  flex-wrap my-32 lg:my-0">
           <div id="profile" className="w-full rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white mx-6 lg:mx-0">
             <div className="p-4 md:p-20 text-center lg:text-left mt-10">
-              <h1 className="text-3xl text-center font-bold pt-8 lg:pt-0">Sobre nós</h1>
+              <h1 className="text-3xl text-center font-bold pt-8 lg:pt-0 animate">Sobre nós</h1>
               <div className="mx-auto lg:mx-0 w-full pt-3 border-b-2 border-green-500 opacity-25"></div>
 
-              <h2 className="text-2xl text-center font-bold pt-8 lg:pt-0 mt-5">Objetivo</h2>
-              <p className="pt-8 text-sm text-left">
+              <h2 className="text-2xl text-center font-bold pt-8 lg:pt-0 mt-5 animate">Objetivo</h2>
+              <p className="pt-8 text-sm text-left animate-left">
                 O projeto "As Casas Contam Histórias" visa se tornar uma ferramenta acessível de educação patrimonial arquitetônica no município de Timóteo,
                 com foco no patrimônio arquitetônico industrial residencial. Busca-se documentar, catalogar e disponibilizar informações sobre as fachadas originais das casas
                 construídas pela siderúrgica ACESITA (atualmente APERAM South America), facilitando o acesso do público às histórias e características dessas construções.
                 O objetivo é promover a conscientização sobre a importância desse patrimônio, incentivando sua preservação e valorização pela comunidade.
               </p>
 
-              <h2 className="text-2xl text-center font-bold pt-8 lg:pt-0 mt-4">Integrantes</h2>
-              <ul className="pt-8 text-sm grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4 mb-3 justify-items-center mx-auto">
+              <h2 className="text-2xl text-center font-bold pt-8 lg:pt-0 mt-8 animate">Integrantes</h2>
+              <ul className="pt-8 text-sm grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-5 mb-3 justify-items-center mx-auto animate-up">
                 <Prof></Prof>
                 <Component nome= "Marcio Gabriel G. Soares" curso="Eng. Computação" funcao="PETiano desde fevereiro de 2024" foto="img/sobre-nos/marcio.png" descricao="Aluno do curso" />
                 <Component nome= "Maria Júlia Loiola Vieira Cotta" curso="Arquitetura e Urbanismo" funcao="PETiano desde fevereiro de 2024" foto="img/sobre-nos/maria.png" descricao="Aluna do curso" />
                 <Component nome= "Larissa Sírio Coelho Penna" curso="Arquitetura e Urbanismo" funcao="PETiano desde fevereiro de 2024" foto="img/sobre-nos/Larissa.png" descricao="Aluna do curso"/>
-                <Component nome= "Felipe" curso="Arquitetura e Urbanismo" funcao="" foto="img/sobre-nos/?.png" descricao="Aluno do curso"/>
-                <Component nome= "Lara" curso="Arquitetura e Urbanismo" funcao="" foto="img/sobre-nos/?.png" descricao="Aluna do curso"/>
+                <Component nome= "Felipe Freitas Moraes" curso="Arquitetura e Urbanismo" funcao="" foto="img/sobre-nos/felipe.png" descricao="Aluno do curso"/>
+                <Component nome= "Lara Gabrielle" curso="Arquitetura e Urbanismo" funcao="" foto="img/sobre-nos/?.png" descricao="Aluna do curso"/>
               </ul>
 
-              <h2 className="text-2xl text-center font-bold pt-8 lg:pt-0">Sobre o PETIT</h2>
-              <p className="pt-8 text-sm text-left">
+              <h2 className="text-2xl text-center font-bold pt-8 lg:pt-0 animate mt-8">Sobre o PETIT</h2>
+              <p className="pt-8 text-sm text-left animate-left">
                 O Programa de Educação Tutorial (PET) é composto por grupos de aprendizagem, orientados, cada um, por um professor tutor, que buscam propiciar aos alunos condições para a realização de atividades extracurriculares que complementem a sua formação acadêmica, procurando atender mais plenamente às necessidades do próprio curso de graduação e/ou ampliar e aprofundar os objetivos e conteúdos programáticos que integram sua estrutura curricular. Desta forma, espera-se promover a formação ampla e de qualidade acadêmica dos alunos de graduação envolvidos direta ou indiretamente com o programa, estimulando a fixação de valores que reforcem a cidadania e a consciência social de todos os participantes para a melhoria dos cursos de graduação.
 
                 O PET é um programa de longo prazo que visa realizar, dentro da universidade brasileira, o modelo de indissociabilidade do ensino, pesquisa e da extensão. Assim, além de um incentivo à melhoria da graduação, o PET pretende estimular a criação de um modelo pedagógico para a universidade, de acordo com os princípios estabelecidos na Constituição Brasileira e na Lei de Diretrizes e Bases da Educação (LDB).
               </p>
 
-              <h2 className="text-2xl text-center font-bold pt-8 lg:pt-0 mt-5">Contato</h2>
-              <section className="bg-white dark:bg-gray-900">
+              <h2 className="text-2xl text-center font-bold pt-8 lg:pt-0 mt-8 animate">Contato</h2>
+              <section className="bg-white dark:bg-gray-900 animate-up">
                 <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                   <form action="#" className="space-y-8">
                     <div>
