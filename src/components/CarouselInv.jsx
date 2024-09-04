fd72c16fd2c065305c5a9027620883203d0e5fe4
Marcio Gabriel <marciogabriel.gs27@gmail.com>
2024-09-04 12:18:41 -0300

    banner galeria

--- src/components/CarouselInv.jsx
+++ src/components/CarouselInv.jsx
@@ -5,17 +5,14 @@ import { Carousel } from "flowbite-react";
 
 export function Component() {
   return (
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+    <div className="h-60 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 1
-        </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 2
-        </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 3
-        </div>
+      <img
+          src="public/img/Banner-Galeria.jpg"
+          alt="Slide 1"
+          className="h-full w-full "
+        />
+       
       </Carousel>
     </div>
   );
