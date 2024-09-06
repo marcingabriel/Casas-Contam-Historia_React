eb7b1f41400e2340bc86d497cdd8977bb195cfa0
Marcio Gabriel <marciogabriel.gs27@gmail.com>
2024-09-06 12:56:20 -0300

    update contato

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Component} from '../components/CardIntegrantes'
 import { Prof } from '../components/CardProf';
 import useAnimateOnScroll from '../components/Animation';
+import emailjs from '@emailjs/browser'
 
 const Sobre = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+
+
+  function sendEmail(){
+    
+    const templateParams={
+      from_name: name,
+      message: message,
+      email: email
+    }
+
+    emailjs.send("service_3j6we7u","template_67m4dey", templateParams,"Hjw4e3W9aJv7H3_UB")
+    .then((response) => {
+      console.log("Sucesso")
+      setName('')
+      setEmail('')
+      setMessage('')
+      alert("Sucesso")
+    })
+
+
+  }
+
+
   useAnimateOnScroll();
+  
   return (
     <div className="font-sans text-gray-900 leading-normal tracking-wider bg-cover">
       <div className="flex items-center justify-center min-h-screen">
@@ -42,18 +71,18 @@ const Sobre = () => {
               <h2 className="text-2xl text-center font-bold pt-8 lg:pt-0 mt-8 animate">Contato</h2>
               <section className="bg-white dark:bg-gray-900 animate-up">
                 <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                  <form action="#" className="space-y-8">
+                  <form action="#" className="space-y-8" onSubmit={sendEmail}>
                     <div>
                       <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nome</label>
-                      <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Seu nome" required />
+                      <input type="text" value={name} id="subject"  className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" onChange={(e) => setName(e.target.value)} placeholder="Seu nome" required />
                     </div>
                     <div>
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-                      <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@example.com" required />
+                      <input type="email" value={email} id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" onChange={(e) => setEmail(e.target.value)} placeholder="name@example.com" required />
                     </div>
                     <div className="sm:col-span-2">
                       <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Mensagem</label>
-                      <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Sua mensagem ..." />
+                      <textarea id="message" value={message} rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" onChange={(e) => setMessage(e.target.value)} placeholder="Sua mensagem ..." />
                     </div>
                     <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Enviar</button>
                   </form>
